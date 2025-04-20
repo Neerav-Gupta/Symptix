ab0c04c914f308fdff8150a0d57d998968c32a3a
Neerav-Gupta <135437722+Neerav-Gupta@users.noreply.github.com>
2025-04-20 14:01:59 -0400

    Update to new AI model

--- src/lib/Chatbot/chatbot.js
+++ src/lib/Chatbot/chatbot.js
@@ -1,7 +1,7 @@
 import { GoogleGenerativeAI } from "@google/generative-ai";
 
 const genAI = new GoogleGenerativeAI("YOUR_API_KEY");
-const model = genAI.getGenerativeModel({ model: "gemini-1.0-pro" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 export async function setup() {
     const prompt = 'You are going to be used as a chatbot which is going to have a back and forth conversation with a patient. The patient is either going to enter some symptoms they have, based on which you have to diagnose those symptoms, without the prompt explicity mentioning it, or they will have a medical related question which you have to answer. Do not answer any question which is not medical related and if the user ask for something of the sort say "I am currently unable to answer that.". To start the conversation, start by saying "Hello, I am the Symptix AI assistant.\nPlease describe your sympoms for me to diagnose you, or enter a health related question!".'
